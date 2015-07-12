f520e8c0649bf078d757efd09314cf99eee780ef
Daniel Collin <daniel@collin.com>
2015-07-12 19:26:36 +0200

    Enable debug logging by default

--- tests/test.c
+++ tests/test.c
@@ -1,10 +1,18 @@
+#define I3__FILE__ "test.c"
+
 #include "tree.h"
+#include "log.h"
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
 	Rect rect = { 0, 0, 1024, 1024 };
+
+	init_logging();
+
+	DLOG("Test\n");
+
 	tree_init(&rect);
 
 	return 0;
