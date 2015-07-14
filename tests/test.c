20ea6687571b3dc083703f4647d1e5ecf5927562
Daniel Collin <daniel@collin.com>
2015-07-14 14:09:19 +0200

    Added callbacks for rendering

--- tests/test.c
+++ tests/test.c
@@ -6,6 +6,7 @@
 #include "output.h"
 #include "con.h"
 #include "render.h"
+#include "callbacks.h"
 #include <stddef.h>
 #include <stdio.h>
 #include <MiniFB.h>
@@ -20,12 +21,60 @@ const char* s_outputFormat = "1024x768+0+0";
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+static uint32_t s_colors[] =
+{
+    0xffb27474,
+    0xffb28050,
+    0xffa9b250,
+    0xff60b250,
+
+    0xff4fb292,
+    0xff4f71b2,
+    0xff8850b2,
+    0xffb25091,
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void fillRect(uint32_t* buffer, Rect rect, int width, uint32_t color)
+{
+    buffer += (rect.y * width) + rect.x;
+
+    for (int y = 0; y < rect.height; ++y)
+    {
+        for (int x = 0; x < rect.width; ++x)
+            buffer[x] = color;
+
+        buffer += width;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static void renderCon(Con* con)
+{
+	static int i = 0;
+
+	fillRect(s_buffer, con->rect, WIDTH, s_colors[i++ & 0x7]);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static DockSysCallbacks callbacks =
+{
+	renderCon,
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int main()
 {
 	Rect rect = { 0, 0, WIDTH, HEIGHT };
 
 	init_logging();
 	tree_init(&rect);
+
+	docksys_set_callbacks(&callbacks);
 
 	fake_outputs_init(s_outputFormat);
 
@@ -34,17 +83,22 @@ int main()
 	Con* workspace = workspace_get("test_ws", NULL);
 
     Con *con = tree_open_con(NULL, NULL);
-    con->layout = L_SPLITH;
-    con_focus(con);
+	tree_split(focused, HORIZ);
 
     con = tree_open_con(NULL, NULL);
-    con->layout = L_SPLITH;
+    con = tree_open_con(NULL, NULL);
+
     con_focus(con);
 
-	tree_render();
+	tree_split(con, VERT);
+    con = tree_open_con(NULL, NULL);
+    con = tree_open_con(NULL, NULL);
 
 	if (!mfb_open("i3 docking test", WIDTH, HEIGHT))
 		return 0;
+
+	tree_render();
+
 
 	for (;;)
 	{
